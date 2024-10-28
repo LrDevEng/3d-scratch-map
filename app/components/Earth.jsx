8f8476cecf029b8c09a85fc00f60ff238c188da9
LrDevEng <lr.deveng@gmail.com>
2024-10-28 15:21:45 +0100

    Add starfield to globe

--- app/components/Earth.jsx
+++ app/components/Earth.jsx
@@ -1,43 +1,62 @@
+/* eslint-disable react/no-unknown-property */
+
 'use client';
 
-import { Line, OrbitControls, useTexture } from '@react-three/drei';
+import {
+  Html,
+  Line,
+  OrbitControls,
+  Shadow,
+  useTexture,
+} from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { ConicPolygonGeometry } from 'three-conic-polygon-geometry';
 import { GeoJsonGeometry } from 'three-geojson-geometry';
 import { v4 as uuid } from 'uuid';
 
-export default function Earth({ countryData, showCoordinateSystem = true }) {
-  const axisStart = 0;
-  const axisEnd = 4;
-  const axisColorX = 'red';
-  const axisColorY = 'blue';
-  const axisColorZ = 'green';
-  const sphereRadius = 2;
-  const sphereResolution = 500;
-  const borderLineRadius = 2.025;
-  const borderLineColor = 'black';
-  const countryPolygonRadiusMin = 2;
-  const countryPolygonRadiusMax = 2.02;
-  const countryPolygonColor = '#f0d897';
-  const orbitControlsMinDist = 2.2;
-  const orbitControlsMaxDist = 8;
+export default function Earth({
+  countryData,
+  showCoordinateSystem = true,
+  axisStart = 0,
+  axisEnd = 4,
+  axisColorX = 'red',
+  axisColorY = 'blue',
+  axisColorZ = 'green',
+  sphereRadius = 2,
+  sphereResolution = 500,
+  borderLineRadius = 2.025,
+  borderLineColor = 'black',
+  countryPolygonRadiusMin = 2,
+  countryPolygonRadiusMax = 2.02,
+  countryPolygonOpacity = 0.75,
+  countryPolygonColor = '#f0d897',
+  countryElevatedColor = 'red',
+  countryElevatedRadius = 2.1,
+  orbitControlsMaxDist = 8,
+  orbitControlsMinDist = 2.5,
+  texturePath = './textures/texture_earth_map_10k.jpg',
+  renderCountryPolygons = true,
+  renderCountryBorders = true,
+}) {
   const ref = useRef();
-  const earthTexture = useTexture('./textures/texture_earth_map_10k.jpg');
+  const earthTexture = useTexture(texturePath);
 
-  console.log(countryData);
-  const france = countryData.features[55];
-  console.log(france);
+  const [selectedCountry, setSelectedCountry] = useState('');
+
+  // console.log(countryData);
+  // const france = countryData.features[55];
+  // console.log(france);
 
   useFrame((state, delta, frame) => {
-    // console.log(countries);
-    // ref.current.rotation.y += delta * 0.05;
+    // console.log(delta);
+    ref.current.rotation.y += delta * 0.05;
     // ref.current.position.z = Math.sin(state.clock.elapsedTime * 4);
   });
 
   return (
     <mesh position={[0, 0, 0]} ref={ref}>
-      {/*Coordinate system*/}
+      {/* Coordinate system */}
       {showCoordinateSystem && (
         <mesh>
           <Line
@@ -67,106 +86,130 @@ export default function Earth({ countryData, showCoordinateSystem = true }) {
         </mesh>
       )}
 
-      {/*Globe sphere*/}
+      {/* Globe sphere */}
       <mesh
         rotation={[0, -Math.PI / 2, 0]}
         onDoubleClick={(event) => {
           event.stopPropagation();
+          setSelectedCountry('');
         }}
       >
         <sphereGeometry
           args={[sphereRadius, sphereResolution, sphereResolution]}
-        />
+        >
+          <Shadow
+            color="blue"
+            colorStop={1}
+            opacity={0.5}
+            fog={false} // Reacts to fog (default=false)
+          />
+        </sphereGeometry>
         <meshStandardMaterial map={earthTexture} />
       </mesh>
 
-      {/*Country polygons*/}
-      {countryData.features.map(({ geometry, properties }) => {
-        if (geometry.type === 'Polygon') {
-          return (
-            <mesh
-              key={`country-${properties.NAME}`}
-              geometry={
-                new ConicPolygonGeometry(
-                  geometry.coordinates,
-                  countryPolygonRadiusMin,
-                  countryPolygonRadiusMax,
-                )
-              }
-              onDoubleClick={(event) => {
-                event.stopPropagation();
-                console.log(event, properties.NAME);
-              }}
-            >
-              <meshBasicMaterial
-                color={
-                  properties.NAME === 'France' ? 'red' : countryPolygonColor
-                }
-                transparent="true"
-                opacity={0.75}
-              />
-            </mesh>
-          );
-        } else if (geometry.type === 'MultiPolygon') {
-          return (
-            <mesh key={`country-${properties.NAME}`}>
-              {geometry.coordinates.map((coordinate, index) => {
-                return (
-                  <mesh
-                    key={`country-multi-polygon-${properties.NAME}-${uuid()}`}
-                    geometry={
-                      new ConicPolygonGeometry(
-                        coordinate,
-                        countryPolygonRadiusMin,
-                        countryPolygonRadiusMax,
-                      )
-                    }
-                    onDoubleClick={(event) => {
-                      event.stopPropagation();
-                      console.log(event, properties.NAME);
-                    }}
-                  >
-                    <meshBasicMaterial
-                      color={
-                        properties.NAME === 'France'
-                          ? 'red'
-                          : countryPolygonColor
-                      }
-                      transparent="true"
-                      opacity={0.75}
-                    />
-                  </mesh>
-                );
-              })}
-            </mesh>
-          );
-        }
-      })}
-
-      {/*Country border*/}
-      <mesh>
-        {countryData.features.map(({ geometry, properties }) => {
+      {/* Country polygons */}
+      {renderCountryPolygons &&
+        countryData.features.map(({ geometry, properties }) => {
+          const radiusMax =
+            selectedCountry === properties.NAME
+              ? countryElevatedRadius
+              : countryPolygonRadiusMax;
+          const countryColor =
+            selectedCountry === properties.NAME
+              ? countryElevatedColor
+              : countryPolygonColor;
           if (geometry.type === 'Polygon') {
             return (
-              <line
-                key={`border-${properties.NAME}`}
-                geometry={new GeoJsonGeometry(geometry, borderLineRadius)}
+              <mesh
+                key={`country-${properties.NAME}`}
+                geometry={
+                  new ConicPolygonGeometry(
+                    geometry.coordinates,
+                    countryPolygonRadiusMin,
+                    radiusMax,
+                  )
+                }
+                onDoubleClick={(event) => {
+                  event.stopPropagation();
+                  setSelectedCountry(properties.NAME);
+                  console.log(event, properties.NAME);
+                }}
               >
-                <lineBasicMaterial color={borderLineColor} />
-              </line>
+                <meshBasicMaterial
+                  color={countryColor}
+                  transparent="true"
+                  opacity={countryPolygonOpacity}
+                />
+                <Html>{properties.NAME}</Html>
+              </mesh>
             );
           } else if (geometry.type === 'MultiPolygon') {
             return (
-              <lineSegments
-                key={`border-${properties.NAME}`}
-                geometry={new GeoJsonGeometry(geometry, borderLineRadius)}
-              >
-                <lineBasicMaterial color={borderLineColor} />
-              </lineSegments>
+              <mesh key={`country-${properties.NAME}`}>
+                {geometry.coordinates.map((coordinate) => {
+                  return (
+                    <mesh
+                      key={`country-multi-polygon-${properties.NAME}-${uuid()}`}
+                      geometry={
+                        new ConicPolygonGeometry(
+                          coordinate,
+                          countryPolygonRadiusMin,
+                          radiusMax,
+                        )
+                      }
+                      onDoubleClick={(event) => {
+                        event.stopPropagation();
+                        setSelectedCountry(properties.NAME);
+                        console.log(event, properties.NAME);
+                      }}
+                    >
+                      <meshBasicMaterial
+                        color={countryColor}
+                        transparent="true"
+                        opacity={countryPolygonOpacity}
+                      />
+                    </mesh>
+                  );
+                })}
+              </mesh>
             );
+          } else {
+            return null;
           }
         })}
-      </mesh>
+
+      {/* Country border */}
+      {renderCountryBorders && (
+        <mesh>
+          {countryData.features.map(({ geometry, properties }) => {
+            const lineRadius =
+              selectedCountry === properties.NAME
+                ? countryElevatedRadius
+                : borderLineRadius;
+            if (geometry.type === 'Polygon') {
+              return (
+                <line
+                  key={`border-${properties.NAME}`}
+                  geometry={new GeoJsonGeometry(geometry, lineRadius)}
+                >
+                  <lineBasicMaterial color={borderLineColor} />
+                </line>
+              );
+            } else if (geometry.type === 'MultiPolygon') {
+              return (
+                <lineSegments
+                  key={`border-${properties.NAME}`}
+                  geometry={new GeoJsonGeometry(geometry, lineRadius)}
+                >
+                  <lineBasicMaterial color={borderLineColor} />
+                </lineSegments>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </mesh>
+      )}
       <OrbitControls
         minDistance={orbitControlsMinDist}
         maxDistance={orbitControlsMaxDist}
--- app/components/Space.jsx
+++ app/components/Space.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/no-unknown-property */
+
 'use client';
 
-import { Preload } from '@react-three/drei';
+import { Preload, Stars } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
-import CartesianCoordinates from './CartesianCoordinates';
 
 const Earth = dynamic(() => import('./Earth'), {
   ssr: false,
@@ -14,8 +15,17 @@ export default function Space({ countryData }) {
   return (
     <Canvas>
       <Suspense fallback={<div>Loading ...</div>}>
-        <ambientLight intensity={3} />
+        <ambientLight intensity={4} />
         <Earth countryData={countryData} />
+        <Stars
+          radius={20}
+          depth={50}
+          count={5000}
+          factor={1}
+          saturation={0}
+          fade={false}
+          speed={0.2}
+        />
         <Preload all />
       </Suspense>
     </Canvas>
